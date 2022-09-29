a2698af2ae17544281a2d63e45f9ca342e0b4b1f
甘春雨 <keyboard3@icloud.com>
2022-09-29 15:41:00 +0800

    fix: linux构建

--- create-app.ts
+++ create-app.ts
@@ -5,14 +5,14 @@ import { copySync } from 'fs-extra'
 
 export async function createApp(config: { packageName: string, appPath: string, packageManager: string, render: string, server: string }) {
   const { render, server, packageName, appPath: projectPath } = config;
-  
+
   copySync(path.join(__dirname, `./templates/project`), projectPath)
   copySync(path.join(__dirname, `./templates/${server}/package.json`), `${projectPath}/package.json`)
 
   changePackageJson(`${projectPath}/package.json`, packageName);
-  
-  spawnSync(`node ${path.join(__dirname, `./templates/${render}/script.js`)}`, { stdio: "inherit", shell: true, env: { PROJECT_PATH: projectPath, PACKAGE_NAME: packageName } });
-  spawnSync(`node ${path.join(__dirname, `./templates/${server}/script.js`)}`, { stdio: "inherit", shell: true, env: { PROJECT_PATH: projectPath, PACKAGE_NAME: packageName } });
+
+  spawnSync(`node ${path.join(__dirname, `./templates/${render}/script.js`)}`, { stdio: "inherit", shell: true, env: { PROJECT_PATH: projectPath, PACKAGE_NAME: packageName, ...process.env } });
+  spawnSync(`node ${path.join(__dirname, `./templates/${server}/script.js`)}`, { stdio: "inherit", shell: true, env: { PROJECT_PATH: projectPath, PACKAGE_NAME: packageName, ...process.env } });
 }
 
 export class DownloadError extends Error { }
