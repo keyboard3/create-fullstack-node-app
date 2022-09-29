ef9d039cb66fa128258da5c7d62899def7281493
甘春雨 <keyboard3@icloud.com>
2022-09-29 09:05:53 +0800

    refactor: 优化构建

--- create-app.ts
+++ create-app.ts
@@ -4,16 +4,15 @@ const path = require("path");
 
 export async function createApp(config: { packageName: string, appPath: string, packageManager: string, render: string, server: string }) {
   const { render, server, packageManager, appPath: projectPath } = config;
-  spawnSync(`mkdir -p ${projectPath}`, { stdio: "inherit", shell: true });
-  spawnSync(`export appPath=${projectPath} && node ${path.join(__dirname, `./templates/${render}/script.js`)}`, { stdio: "inherit", shell: true });
-  spawnSync(`export appPath=${projectPath} && node ${path.join(__dirname, `./templates/${server}/script.js`)}`, { stdio: "inherit", shell: true });
+  
   spawnSync(`cp \
   ${path.join(__dirname, `./templates/project/*`)} \
   ${path.join(__dirname, `./templates/${server}/package.json`)} \
   ${projectPath}`, { stdio: "inherit", shell: true })
-
   changePackageJson(`${projectPath}/package.json`, projectPath);
-  spawnSync(`cd ${projectPath} && ${packageManager} install`, { stdio: "inherit", shell: true })
+  
+  spawnSync(`export appPath=${projectPath} && node ${path.join(__dirname, `./templates/${render}/script.js`)}`, { stdio: "inherit", shell: true });
+  spawnSync(`export appPath=${projectPath} && node ${path.join(__dirname, `./templates/${server}/script.js`)}`, { stdio: "inherit", shell: true });
 }
 
 export class DownloadError extends Error { }
--- templates/nest/script.js
+++ templates/nest/script.js
@@ -3,7 +3,7 @@ const { changePackageJson } = require("../common/utils");
 const path = require("path");
 const projectPath = process.env.appPath;
 //创建项目
-spawnSync(`cd ${projectPath} && git clone https://github.com/nestjs/typescript-starter.git server`, { stdio: "inherit", shell: true });
+spawnSync(`cd ${projectPath} && nest new server`, { stdio: "inherit", shell: true });
 
 //将文件覆盖进去
 spawnSync(`cp \
