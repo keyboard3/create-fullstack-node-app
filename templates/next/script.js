25884cbe1dc4f3117aa69f856869b420edf99c3f
甘春雨 <keyboard3@icloud.com>
2022-09-29 09:11:17 +0800

    fix: 删除多余的移动node_modules的代码

--- templates/next/script.js
+++ templates/next/script.js
@@ -6,8 +6,6 @@ const projectPath = process.env.appPath;
 //创建项目
 spawnSync(`cd ${projectPath} && npx create-next-app@latest --typescript render`, { stdio: "inherit", shell: true });
 
-//将文件覆盖进去
-spawnSync(`cd ${projectPath} && mv render/node_modules ./ && mv render/yarn.lock ./`, { stdio: "inherit", shell: true });
 spawnSync(`cp ${path.join(__dirname, "index.js")} ${projectPath}/render/`, { stdio: "inherit", shell: true });
 spawnSync(`cp ${path.join(__dirname, "page.tsx")} ${projectPath}/render/pages/index.tsx`, { stdio: "inherit", shell: true });
 
