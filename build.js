3559207536a7ba999704de14b06224e727cf7999
甘春雨 <keyboard3@icloud.com>
2022-10-22 20:49:47 +0800

    fix: 对script文件进行打包

--- build.js
+++ build.js
@@ -1,8 +1,20 @@
 const { spawnSync } = require("child_process");
 const fse = require("fs-extra");
+const fs = require("fs")
 spawnSync(`esbuild index.ts --bundle --outfile=dist/index.js --platform=node`, {
   stdio: "inherit",
   shell: true,
 });
 
 fse.copySync(`${__dirname}/templates`, `${__dirname}/dist/templates`);
+
+const dirs = fs.readdirSync(`${__dirname}/dist/templates`)
+dirs.forEach(dir => {
+  const scriptFilename = `${__dirname}/dist/templates/${dir}/script.js`
+  if (fs.existsSync(scriptFilename)) {
+    spawnSync(`esbuild ${scriptFilename} --bundle --outfile=${scriptFilename} --platform=node --allow-overwrite`, {
+      stdio: "inherit",
+      shell: true,
+    });
+  }
+})
